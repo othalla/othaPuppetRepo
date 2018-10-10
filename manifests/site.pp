867d3a13e852123725e994ffee816700b12612e8
othalla <othalla.lf@gmail.com>
2018-10-11 00:33:46 +0200

    Enance Nginx & Letsencrypt config for tautulli

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,13 +17,25 @@ node 'rctn001.int.othalland.xyz' {
 }
 
 node 'rctn004.int.othalland.xyz' {
+  class { ::nginx:
+    confd_purge  => true,
+    server_purge => true,
+  }
   class { ::letsencrypt:
     email => 'othalla.lf@gmail.com',
   }
-  include nginx
-  nginx::resource::server { 'tautulli':
-    ensure               => present,
-    server_name          => ['tautulli', 'tautulli.int.othalland.xyz'],
+  letsencrypt::certonly { 'tautulli.othalland.xyz':
+    domains         => ['tautulli.othalland.xyz'],
+    plugin          => 'standalone',
+    additional_args => ['--preferred-challenges http'],
+  }
+  nginx::resource::server { 'tautulli.othalland.xyz':
+    ensure      => present,
+    server_name => ['tautulli.othalland.xyz'],
+    listen_port => 443,
+    ssl         => true,
+    ssl_cert    => '/etc/letsencrypt/live/tautulli.othalland.xyz/fullchain.pem',
+    ssl_key     => '/etc/letsencrypt/live/tautulli.othalland.xyz/privkey.pem',
   }
 }
 
