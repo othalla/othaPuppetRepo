3521eb4a6ea47993663bae716b4b910c0b7a1c8f
othalla <othalla.lf@gmail.com>
2018-10-10 23:30:23 +0200

    Add first nginx & letsencrypt config for Tautulli RP

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,3 +15,15 @@ node 'r002.int.othalland.xyz' {
 node 'rctn001.int.othalland.xyz' {
   include ::roles::tautulli
 }
+
+node 'rctn004.int.othalland.xyz' {
+  class { ::letsencrypt:
+    email => 'othalla.lf@gmail.com',
+  }
+  include nginx
+  nginx::resource::server { 'tautulli':
+    ensure               => present,
+    server_name          => ['tautulli', 'tautulli.int.othalland.xyz'],
+  }
+}
+
