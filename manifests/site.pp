9c6bf78a32ca8ff47c9cd75ed8bdda6ffb688a7c
othalla <othalla.lf@gmail.com>
2018-10-13 22:52:04 +0200

    Add full nginx reverse proxy config for tautulli

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,12 +30,38 @@ node 'rctn004.int.othalland.xyz' {
     additional_args => ['--preferred-challenges http'],
   }
   nginx::resource::server { 'tautulli.othalland.xyz':
+    ensure               => present,
+    server_name          => ['tautulli.othalland.xyz'],
+    listen_port          => 443,
+    ssl                  => true,
+    ssl_cert             => '/etc/letsencrypt/live/tautulli.othalland.xyz/fullchain.pem',
+    ssl_key              => '/etc/letsencrypt/live/tautulli.othalland.xyz/privkey.pem',
+    use_default_location => false,
+  }
+  -> nginx::resource::location { 'Tautulli_letsencrypt':
     ensure      => present,
-    server_name => ['tautulli.othalland.xyz'],
-    listen_port => 443,
+    location    => '/.well-known/acme-challenge',
     ssl         => true,
-    ssl_cert    => '/etc/letsencrypt/live/tautulli.othalland.xyz/fullchain.pem',
-    ssl_key     => '/etc/letsencrypt/live/tautulli.othalland.xyz/privkey.pem',
+    ssl_only    => true,
+    server      => 'tautulli.othalland.xyz',
+    www_root    => '/var/www/html',
+    index_files => [],
+    priority    => 401,
+  }
+  -> nginx::resource::location { 'Tautulli_root':
+    ensure           => present,
+    location         => '/',
+    ssl              => true,
+    ssl_only         => true,
+    server           => 'tautulli.othalland.xyz',
+    proxy            => 'http://192.168.1.120:8181',
+    proxy_set_header => [
+      'Host $host',
+      'X-Real-IP $remote_addr',
+      'X-Forwarded-Host $server_name',
+      'X-Forwarded-For $proxy_add_x_forwarded_for',
+      'X-Forwarded-Proto $scheme',
+      'X-Forwarded-Ssl on'],
+    priority         => 402,
   }
 }
-
