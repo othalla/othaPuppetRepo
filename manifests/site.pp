cc5763ef99f10fc6273ba1adf93992da9ae096c5
othalla <othalla.lf@gmail.com>
2019-01-01 11:28:08 +0100

    Add grafana class for rctn005

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,3 +23,10 @@ node 'rctn003.int.othalland.xyz' {
 node 'rctn004.int.othalland.xyz' {
   include profiles::reverse_proxy
 }
+
+node 'rctn005.int.othalland.xyz' {
+  class { 'grafana':
+    install_method => 'package',
+    package_source => 'https://dl.grafana.com/oss/release/grafana_5.4.2_armhf.deb',
+  }
+}
