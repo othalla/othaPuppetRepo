ae118880bcd52b6caee4f4d934cedcf88eed5aee
othalla <othalla.lf@gmail.com>
2019-05-09 23:21:38 +0200

    Set server profile to all nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,10 +17,12 @@ node 'rctn001.int.othalland.xyz' {
 }
 
 node 'rctn003.int.othalland.xyz' {
-  include profiles::prometheus_server
+  include ::profiles::server
+  include ::profiles::prometheus_server
 }
 
 node 'rctn005.int.othalland.xyz' {
+  include ::profiles::server
   class { 'grafana':
     install_method => 'package',
     package_source => 'https://dl.grafana.com/oss/release/grafana_6.1.4_armhf.deb',
@@ -28,13 +30,16 @@ node 'rctn005.int.othalland.xyz' {
 }
 
 node 'rctn011.int.othalland.xyz' {
+  include ::profiles::server
   include ::profiles::dns
 }
 
 node 'rctn012.int.othalland.xyz' {
+  include ::profiles::server
   include ::profiles::dhcp
 }
 
 node 'rctn013.int.othalland.xyz' {
+  include ::profiles::server
   class { 'puppetdb': }
 }
